5767306713f62026278d0e53606e91d5d70b659a
auddl0756 <cjddl789123@naver.com>
2021-09-28 09:19:57 +0900

    iterator 패턴- ArrayList 버전

--- src/part1/iterator/BookShelf.java
+++ src/part1/iterator/BookShelf.java
@@ -1,23 +1,27 @@
 package part1.iterator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookShelf {
-    private Book[] books;
-    private int last;
-    public BookShelf(int maxSize){
-        this.books = new Book[maxSize];
+    private List<Book> books;
+
+    public BookShelf(){
+        this.books = new ArrayList<>();
     }
 
     public Book getBook(int index){
-        return books[index];
+        //return books[index];
+        return books.get(index);
     }
 
     public void addBook(Book book){
-        this.books[last] = book;
-        last++;
+        //this.books[last] = book;
+        this.books.add(book);
     }
 
     public int getLength(){
-        return last;
+        return books.size();
     }
 
     public Iterator iterator(){
--- src/part1/iterator/Main.java
+++ src/part1/iterator/Main.java
@@ -2,7 +2,7 @@ package part1.iterator;
 
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
         bookShelf.addBook(new Book("java"));
         bookShelf.addBook(new Book("javascript"));
         bookShelf.addBook(new Book("mysql"));
